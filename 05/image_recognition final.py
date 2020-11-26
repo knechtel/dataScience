2119ecf99d43880c63d07d450b96819632b95d97
maiquelknechtel <maiquelknechtel@gmail.com>
2020-11-26 14:37:58 -0300

    Modificacoes para funcionar em versao nova.

--- 05/image_recognition final.py
+++ 05/image_recognition final.py
@@ -1,9 +1,9 @@
 import numpy as np
-from keras.preprocessing import image
-from keras.applications import resnet50
-
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
 # Load Keras' ResNet50 model that was pre-trained against the ImageNet database
-model = resnet50.ResNet50()
+model = ResNet50()
 
 # Load the image file, resizing it to 224x224 pixels (required by this model)
 img = image.load_img("bay.jpg", target_size=(224, 224))
@@ -15,13 +15,13 @@ x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
 
 # Scale the input image to the range used in the trained network
-x = resnet50.preprocess_input(x)
+x = preprocess_input(x)
 
 # Run the image through the deep neural network to make a prediction
 predictions = model.predict(x)
 
 # Look up the names of the predicted classes. Index zero is the results for the first image.
-predicted_classes = resnet50.decode_predictions(predictions, top=9)
+predicted_classes = decode_predictions(predictions, top=9)
 
 print("This is an image of:")
 
